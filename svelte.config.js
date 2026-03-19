a0f385ec18d2e698723049446777b05243a9269f
shumon83 <119336930+shumon83@users.noreply.github.com>
2026-03-20 01:00:16 +0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
   kit: {
     adapter: adapter()
   }
 };
+
+export default config;
